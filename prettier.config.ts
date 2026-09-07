68d9c1dbc0e9498321c8052ec3d12d2b5ef8ce5b
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-09-07 16:04:05 +0530

    Use prettier.config.ts instead of .json

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+    trailingComma: "all",
+    tabWidth: 4,
+    printWidth: 120,
+    endOfLine: "lf",
+    singleQuote: false,
+};
+
+export default config;
